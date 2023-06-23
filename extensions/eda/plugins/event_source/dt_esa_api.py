1c2a4aa02c162103932fecc920d45b1e088f6b35
Jai28 <techj2002@yahoo.com>
2023-06-23 12:22:53 -0500

    Changed some code for formatting issue

--- extensions/eda/plugins/event_source/dt_esa_api.py
+++ extensions/eda/plugins/event_source/dt_esa_api.py
@@ -1,53 +1,48 @@
-import asyncio
-import aiohttp
-import logging
-import json
-
+"""Plugin pulls Dynatrace detected problems and sends it to the EDA server."""
 from typing import Any, Dict
+import aiohttp
+import asyncio
+import logging
 
-async def getProblems(dt_host,dt_token):
+async def getproblems(dt_host: str,dt_token: str):
+    """Pull Dynatrace detected problems from Dynatrace Problems API."""
     timeout1 = aiohttp.ClientTimeout(total=30)
-    async with aiohttp.ClientSession(headers={"Authorization": f"Api-Token {dt_token}"},timeout=timeout1) as session:
-        #dt_tag_encode = urllib.parse.urlencode(dt_tag)
+    async with aiohttp.ClientSession(
+        headers=
+        {"Authorization": f"Api-Token {dt_token}"},timeout=timeout1) as session:
         url = f"{dt_host}/api/v2/problems?fields=recentComments&from=now-10m&to=now"
         async with session.get(url) as resp:
-
             try:
                 return await resp.json()
             except Exception as err:
-                logging.exception(err)
+                logging.exception("Error" + err.message)
                 logging.warning(resp.status_code)
                 logging.warning(resp.text)
-                return {}  
+                return {}
 
-async def updateDTProblem(prob_id,dtAPIHost,dtAPIToken):
+async def updatedtproblem(prob_id: str,dtapihost: str,dtapitoken: str):
+    """Updates problem comment once its sent to the EDA server."""
     timeout1 = aiohttp.ClientTimeout(total=30)
-    async with aiohttp.ClientSession(headers={"Authorization": f"Api-Token {dtAPIToken}"},timeout=timeout1) as session:
+    async with aiohttp.ClientSession(
+        headers={"Authorization": f"Api-Token {dtapitoken}"},timeout=timeout1) as session:
 
-        url = f"{dtAPIHost}/api/v2/problems/{prob_id}/comments"
+        url = f"{dtapihost}/api/v2/problems/{prob_id}/comments"
         commentbody = {}
         commentbody["context"] = "Event Driven Ansible"
         commentbody["message"] = "Sent to EDA Server"
-    
-        #print(commentbody)
         r = await session.post(url,json=commentbody)
         if r.status != 201:
-            logging.warn(r.status_code)
-            logging.warn(r.text)
+            logging.warning(r.status_code)
+            logging.warning(r.text)
 
-async def main(queue: asyncio.Queue, args: Dict[str, Any]):
-
+async def main(queue: asyncio.Queue, args: Dict[str,Any]):
+    """Processes the problem information."""
     dt_api_host = args.get("dt_api_host")
     dt_api_token = args.get("dt_api_token")
-    #dt_entity_tag = args.get("dt_entity_tags")
-
     delay = int(args.get("delay", 40))
-
     while True:
-        #try:
-        problems = await getProblems(dt_api_host,dt_api_token)
+        problems = await getproblems(dt_api_host,dt_api_token)
         for problem in problems.get("problems"):
-            #async with session.get(problem) as resp:
             pr_comment = problem.get("recentComments").get("comments")
             commentcount = 0
             for comm in pr_comment:
@@ -55,16 +50,12 @@ async def main(queue: asyncio.Queue, args: Dict[str, Any]):
                 if("EDA" in contents):
                     #ignore this problem
                     commentcount = commentcount + 1
-                
-            if(commentcount > 0):
-                print("This problem has already been sent to EDA server")
+            if commentcount > 0:
                 logging.info("This problem has already been sent to EDA server")
             else:
                 prob_id = problem.get("problemId")
-
                 await queue.put(problem)
-
                 # Once sent update comment to "Sent to EDA server"
-                await updateDTProblem(prob_id,dt_api_host,dt_api_token)
-            
+                await updatedtproblem(prob_id,dt_api_host,dt_api_token)
+
         await asyncio.sleep(delay)
